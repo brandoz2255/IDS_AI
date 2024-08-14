e85588ebfcc409c4ea66438ae9a60ea7c3edba12
Dulc3 <124217011+brandoz2255@users.noreply.github.com>
2024-08-13 21:57:37 -0700

    added a reading/creating pcap files feature with a TUI

--- backend/src/collector.py
+++ backend/src/collector.py
@@ -2,6 +2,7 @@ import scapy.all as scapy
 import socket
 import threading
 import netifaces
+import os
 
 
 """
@@ -17,39 +18,93 @@ This module will be responsible for gathering network data that needs to be anal
 - have it a wireshark/nmap combination 
 	- might need to be split up into two modules 
 
+
+             (
+                .            )        )
+                         (  (|              .
+                     )   )\/ ( ( (
+             *  (   ((  /     ))\))  (  )    )
+           (     \   )\(          |  ))( )  (|
+           >)     ))/   |          )/  \((  ) 
+           (     (      .        -.     V )/   )(    (
+            \   /     .   \            .       \))   ))
+              )(      (  | |   )            .    (  /
+             )(    ,'))     \ /          \( `.    )
+             (\>  ,'/__      ))            __`.  /
+            ( \   | /  ___   ( \/     ___   \ | ( (
+             \.)  |/  /   \__      __/   \   \|  ))
+            .  \. |>  \      | __ |      /   <|  /
+                 )/    \____/ :..: \____/     \ <
+          )   \ (|__  .      / ;: \          __| )  (
+         ((    )\)  ~--_     --  --      _--~    /  ))
+          \    (    |  ||               ||  |   (  /
+                \.  |  ||_             _||  |  /
+                  > :  |  ~V+-I_I_I-+V~  |  : (.
+                 (  \:  T\   _     _   /T  : ./
+                  \  :    T^T T-+-T T^T    ;<
+                   \..`_       -+-       _'  )
+         )            . `--=.._____..=--'. ./
+
   """
 
-class DataCollectionModule:
 
+
+class DataCollectionModule:
     def __init__(self, interface='wlp4s0'):
         self.interface = interface
         self.devices = []
 
 
     def list_interfaces(self):
-        """WILL LIST ALL AVAILABLE NETWORK INTERFACES"""
+        """ WILL LIST ALL AVAILBLE NETWORK INTERFACES """
+
+
         interfaces = netifaces.interfaces()
         print("Available network interfaces:")
         for iface in interfaces:
             print(iface)
 
 
-    def capture_packets(self, packet_count=10):
+
+
+    def capture_packets(self, packet_count=20, save_to_file=None):
         """Capture live network traffic."""
+
+
         print(f"Capturing {packet_count} packets on {self.interface}...")
         packets = scapy.sniff(iface=self.interface, count=packet_count)
+        
+
+        if save_to_file:
+
+
+            save_dir = os.path.dirname(save_to_file)
+
+            
+            if save_dir and not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+                print(f"Directory {save_dir} created.")
+
+
+            scapy.wrpcap(save_to_file, packets)
+            print(f"Captured packets saved to {save_to_file}")
+        
+
         for packet in packets:
             print(packet.summary())
+
+        """ wrpcap is responsible for saving pcap files from my network """
 
 
     def scan_network(self, ip_range):
         """Scan devices connected to a network."""
+
+
         print(f"Scanning devices in the network range: {ip_range}")
         arp_request = scapy.ARP(pdst=ip_range)
         broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
         arp_request_broadcast = broadcast/arp_request
         answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-
         
         devices = []
         for element in answered_list:
@@ -61,6 +116,8 @@ class DataCollectionModule:
 
     def process_logs(self, log_file):
         """Process network logs."""
+
+
         print(f"Processing log file: {log_file}")
         with open(log_file, 'r') as file:
             logs = file.readlines()
@@ -68,24 +125,47 @@ class DataCollectionModule:
                 print(log.strip())
 
 
-    def run(self):
-        """Run the data collection module."""
-        capture_thread = threading.Thread(target=self.capture_packets)
-        capture_thread.start()
+    def scan_captured_traffic(self, pcap_file):
+        """ Scans already captured network traffic """
 
 
-        scan_thread = threading.Thread(target=self.scan_network, args=("192.168.1.0/24",))
-        scan_thread.start()
+        print(f"Scanning Packets from {pcap_file} ......")
+        packets = scapy.rdpcap(pcap_file)
+        for packet in packets:
+            print(packet.summary())
 
 
-        capture_thread.join()
-        scan_thread.join()
+    def run(self, pcap_file=None, save_to_file=None):
+        """Runs the data collection module. BEEP BOOP BOP!"""
+
+
+        if pcap_file:
+            self.scan_captured_traffic(pcap_file)
+
+        else:
+            capture_thread = threading.Thread(target=self.capture_packets, kwargs={'save_to_file': save_to_file})
+            capture_thread.start()
+
+
+            scan_thread = threading.Thread(target=self.scan_network, args=("192.168.1.0/24",))
+            scan_thread.start()
+
+            capture_thread.join()
+            scan_thread.join()
+
+        
+""" main Function duhh sillies !  """
+
 
 if __name__ == "__main__":
     data_collector = DataCollectionModule(interface='wlp4s0')
     data_collector.list_interfaces()
-    
-    
 
-    # Now, run the data collection tasks
-    data_collector.run()
+    # Added options: capture live traffic, save to a file, or analyze a pcap file
+    pcap_file = input("-->  |Enter path to pcap file to analyze (or leave empty to capture live traffic): ").strip()
+    if pcap_file:
+        data_collector.run(pcap_file=pcap_file)
+    else:
+        print("-----------------------------------------------------------------------------------------------------------------------------")
+        save_to_file = input("-->  |Enter path to save captured packets (or leave empty to not save): ").strip()
+        data_collector.run(save_to_file=save_to_file)
